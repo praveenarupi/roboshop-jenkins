a8d6a41d22eb49dd75486ef67e68fb4f24e2921c
praveenarupi <praveenarupi@gmail.com>
2022-11-15 15:10:18 +0530

    updating ./roboshop-jenkins

--- jobs-create/main.tf
+++ jobs-create/main.tf
@@ -17,3 +17,12 @@ resource "jenkins_folder" "folder" {
   name = element(var.folders, count.index)
   description = element(var.folders, count.index)
 }
+
+resource "jenkins_job" "infra-jobs" {
+  count = length(var.infra-jobs)
+  name  = element(var.infra-jobs, count.index)
+  folder = "infrastructure"
+  template = templatefile("${path.module}/pipeline-job.xml", {
+    git_repo = element(var.infra-jobs, count.index)
+  })
+}
