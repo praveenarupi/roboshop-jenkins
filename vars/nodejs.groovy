380a2018cc67a57b6c500d85e8c246f8ee07c27e
praveenarupi <praveenarupi@gmail.com>
2022-11-17 19:31:47 +0530

    updating ./roboshop-jenkins

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -7,12 +7,29 @@ def call() {
         }
 
         stages {
+
             stage('Download Dependencies') {
             steps {
                 sh ''' 
                     npm install                    
                   '''
             }
+            }
+
+            stage('Code Quality') {
+                steps {
+                    sh ''' 
+                    echo Code                    
+                  '''
+                }
+            }
+
+            stage('Make Release - Upload Artifacts') {
+                steps {
+                    sh ''' 
+                    echo artifacts                    
+                  '''
+                }
             }
         }
         post {
