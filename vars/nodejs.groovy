abaa54fb6393c2f5b4a0d172b7ae71e6c20ef281
praveenarupi <praveenarupi@gmail.com>
2022-11-18 11:50:44 +0530

    updating ./roboshop-jenkins

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -28,7 +28,7 @@ def call() {
                 }
                 steps {
                     sh ''' 
-                    sonar-scanner -Dsonar.host.url=http://172.31.76.87:9000 -Dsonar.login=${SONAR_USR} -D sonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} -Dsonar-qualitygate.wait=true              
+                    sonar-scanner -Dsonar.host.url=http://172.31.76.87:9000 -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} -Dsonar.projectKey=${COMPONENT} -Dsonar.qualitygate.wait=true
                   '''
                 }
             }
